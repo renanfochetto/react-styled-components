c4ce82c3ffe0ac2157d17f4655dbef05ce8f460e
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-01-15 19:40:53 -0300

    Criação e configuração das tags e da galeria de fotos.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Banner from "./components/Banner/index.jsx";
 import bannerBackground from "./assets/banner.png";
 import Galeria from "./components/Galeria/index.jsx";
 
+import fotos from './fotos.json';
+import {useState} from "react";
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244f 48%, #154580 96.76%);
@@ -33,7 +35,8 @@ const ConteudoGaleria = styled.section`
 
 
 
-function App() {
+const App = () => {
+    const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
     return (
         <FundoGradiente>
             <EstilosGlobais/>
@@ -46,7 +49,7 @@ function App() {
                             texto="A galeria mais completa de fotos do espaço!"
                             backgroundImage={bannerBackground}
                         />
-                        <Galeria/>
+                        <Galeria fotos={fotosDaGaleria}/>
                     </ConteudoGaleria>
                 </MainContainer>
             </AppContainer>
--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,6 +1,42 @@
-const Galeria = () => {
+import {styled} from "styled-components"
+import Titulo from "../Titulo"
+import Tags from "./Tags"
+import Populares from "./Populares"
+import Imagem from "./Imagem"
+
+const GaleriaContainer = styled.div`
+    display: flex;
+    gap: 24px;
+`
+
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`
+
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({fotos = []}) => {
     return (
-        <h2>Navegue pela galeria</h2>
+        <>
+            <Tags/>
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem
+                            key={foto.id}
+                            foto={foto}/>)
+                        }
+                    </ImagensContainer>
+                </SecaoFluida>
+                <Populares/>
+            </GaleriaContainer>
+        </>
     )
 }
 
