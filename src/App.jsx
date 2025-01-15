95eb1369778431e583f9a53a856c21ee3f06fb9a
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-01-15 18:02:01 -0300

    Criação e configuração dos componentes Cabeçalho, Barra Lateral e Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import EstilosGlobais from "./components/EstilosGlobais/index.jsx";
+import Cabecalho from "./components/Cabecalho/index.jsx";
+import BarraLateral from "./components/BarraLateral/index.jsx";
+import Banner from "./components/Banner/index.jsx";
+import bannerBackground from "./assets/banner.png";
+
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244f 48%, #154580 96.76%);
@@ -11,6 +16,9 @@ function App() {
     return (
         <FundoGradiente>
             <EstilosGlobais />
+            <Cabecalho />
+            <Banner texto="A galeria mais completa de fotos do espaço!" backgroundImage={bannerBackground} />
+            <BarraLateral />
         </FundoGradiente>
     )
 }
